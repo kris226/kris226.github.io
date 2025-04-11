960038d03240d05e3716da53ea3fc38d6c3ecdce
kris226 <krystof@mikulkovi.cz>
2025-04-11 21:23:38 +0200

    add game 2

--- clicker.js
+++ clicker.js
@@ -4,16 +4,18 @@ let power = 1;
 function changeScore()
 {
     count = count + power;
-    document.getElementById("score").innerText = `Clicked: ${count} times`;    
+    document.getElementById("score").innerText = `Coins ${count}`;    
 }
 
 
 function upgrade()
 {
-    
+    if (count >= 10){
     power++;
+    document.getElementById("powerText").innerText = `Power ${power}`;
     count = count - 10;
-    document.getElementById("score").innerText = `Clicked: ${count} times`;
+    document.getElementById("score").innerText = `Coins ${count}`;
+    }
 }
 
 
