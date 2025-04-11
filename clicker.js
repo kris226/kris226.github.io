5c10ab8b656dbfaae5a873db531e457ce5d0c015
kris226 <krystof@mikulkovi.cz>
2025-04-11 21:02:27 +0200

    add game1

--- clicker.js
+++ clicker.js
@@ -1,9 +1,24 @@
+let count = 0;
+let power = 1;
 
 function changeScore()
 {
-    document.getElementById("score").innerText += "I";    
+    count = count + power;
+    document.getElementById("score").innerText = `Clicked: ${count} times`;    
+}
+
+
+function upgrade()
+{
+    
+    power++;
+    count = count - 10;
+    document.getElementById("score").innerText = `Clicked: ${count} times`;
 }
 
 
 
+
 document.getElementById("clicker").addEventListener("click", changeScore);
+
+document.getElementById("upgrador").addEventListener("click", upgrade);
